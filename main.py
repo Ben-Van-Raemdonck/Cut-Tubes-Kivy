8420331b5ee39de7a2316b7edebc84ffbc8f8efd
Ben-Van-Raemdonck <Ben-vanraemdonck@hotmail.com>
2021-09-02 22:15:37 +0200

    add plyer filechooser

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from plyer import filechooser
 from kivymd.toast import toast
-from kivymd.uix.filemanager import MDFileManager
 
 
 class MainApp(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.file_manager = MDFileManager(
-            exit_manager=self.exit_manager,
-            select_path=self.select_path,
-        )
 
     def build(self):
         self.theme_cls.theme_style = 'Dark'
         self.theme_cls.primary_palette = 'Blue'
         return Builder.load_file('main.kv')
 
-    def file_manager_open(self):
-        self.file_manager.show('/')
-
-    def exit_manager(self):
-        self.file_manager.close()
-
-    def select_path(self, path):
-        self.exit_manager()
-        toast(path)
+    def choose_file(self):
+        path = filechooser.open_file(title='choose a bom list', filters=[('Excel file', '*.xlsx')])
+        toast(path[0])
 
 
 if __name__ == '__main__':
